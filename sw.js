f8089a6b59978c77ba62b6cfedeabebb453dbfb4
artuasova <a9173475441@gmail.com>
2025-07-23 15:31:42 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,44 @@
-self.addEventListener('install', e => e.waitUntil(caches.open('static').then(c => c.addAll(['.', 'index.html', 'style.css', 'app.js', 'manifest.json']))) ;
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = 'static-v2';
+const FILES_TO_CACHE = [
+  '.', 
+  'index.html', 
+  'style.css', 
+  'app.js', 
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', evt => {
+  console.log('[ServiceWorker] Install');
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  console.log('[ServiceWorker] Activate');
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(response => response || fetch(evt.request))
+  );
+});
